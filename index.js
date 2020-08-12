b0e16132a3bfebc54283e1f2feb9f03f7ecd0c7d
Salma Elshahawy <salmaeng71@gmail.com>
2020-08-12 11:45:57 +0000

    fix mutate array

--- index.js
+++ index.js
@@ -1,18 +1,20 @@
-function mutateTheArray(a, n){
-	let b = new Array(n);
-
-	for(let i = 0; i <= n-1; i++){
-		if(a[i - 1] === undefined){
-			a[i - 1] = 0;
-		} else if(a[i + 1] === undefined){
-			a[i + 1] = 0
-		}
-		b[i] = a[i - 1] + a[i] + a[i + 1]
-		}
-	return b;
+function mutateTheArray(n, a) {
+    if(n > 1){
+        let b = new Array(n)
+            for (let i = 0; i <= n-1; i++){
+                if(a[i-1] === undefined){
+                    a[i-1] = 0;
+                } else if(a[i+1] === undefined){
+                    a[i+1] = 0
+                }
+                b[i] = a[i-1]+a[i]+a[i+1]
+            }
+        return b
+    }
+    return a;
 }
 
-const a = [4 ,-5 ,0 ,-1 ,1]
+const a = [4 ,0 ,1 ,-2 ,3]
 const n = 5
 
-console.log(mutateTheArray(a, n))
+console.log(mutateTheArray(n, a))
